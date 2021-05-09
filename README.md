5fcca07799606938e6515ba3bd655b765f899d43
SocioDroid <sociodroid2019@gmail.com>
2021-05-09 17:16:19 +0530

    Solved merge-k-sorted-lists

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Leetcode Solutions
 My accepted leetcode solutions in C++
 
-
+|NO.|Title|Solution|
+|01|test|[C++]()
---
+++ generate_readme.py
@@ -0,0 +1,33 @@
+from os import listdir
+from os.path import isfile, join
+from collections import OrderedDict
+
+onlyfiles = [f for f in listdir(".") if isfile(join(".", f))]
+solvedProblems = OrderedDict()
+
+readme='''
+# Leetcode Solutions
+My accepted leetcode solutions in C++
+
+|NO.|Title|Solution|
+|---|-----|--------|
+'''
+for i in onlyfiles:
+    namesplit = i.split('.')
+    if len(namesplit)==3:        
+        solvedProblems[int(namesplit[0])] =   [" ".join(namesplit[1].split("-")),"https://leetcode.com/problems/"+namesplit[1],i]
+
+for key, value in solvedProblems.items():
+    readme+="|{}|[{}]({})|[C++]({})|\n".format(key,value[0],value[1],value[2])
+
+print(readme)
+
+import glob
+import os
+
+list_of_files = glob.glob('*.cpp') # * means all if need specific format then *.csv
+latest_file = max(list_of_files, key=os.path.getmtime)
+
+os.system("git add .")
+os.system("git commit -m \"Solved "+latest_file.split('.')[1]+"\"")
+os.system("git push")
